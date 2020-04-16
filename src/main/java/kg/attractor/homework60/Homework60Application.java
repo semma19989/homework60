7eb16b5c7f959367eed23c8bec646beeec9b7224
Semma <semm1998>
2020-04-16 18:33:57 +0600

    Homework60 java ant javasript Homework61

---
+++ src/main/java/kg/attractor/homework60/Homework60Application.java
@@ -0,0 +1,13 @@
+package kg.attractor.homework60;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework60Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework60Application.class, args);
+    }
+
+}
---
+++ src/test/java/kg/attractor/homework60/Homework60ApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.attractor.homework60;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework60ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
